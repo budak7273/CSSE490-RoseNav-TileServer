f66ecfa9daefcb5e40d5fa29ce8a3290047c36b4
Robb <computerguy440+gh@gmail.com>
2021-11-15 14:36:09 -0500

    Use CORS to appease the powers that be

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 
 const caching = require("./scripts/caching");
 
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const path = require('path');
+const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
